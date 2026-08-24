8c3270fd725dc9132148ebc91a8728c9a060353b
willaanov <willanovansatritamma@gmail.com>
2026-08-24 18:59:44 +0700

    be 09

--- jelajahpo-backend/server.js
+++ jelajahpo-backend/server.js
@@ -3,11 +3,43 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jelajahpo_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err)
+    } else {
+        console.log('Berhasil konek ke database JelajahPo');
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Selamat datang di JelajahPo API!');
 });
 
+app.get('/wisata', (req, res) => {
+    const sql = 'SELECT * FROM wisata';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results); 
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server JelajahPo jalan di http://localhost:${PORT}`);
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results); 
+    });
 });
